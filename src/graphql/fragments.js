06724eb0ba1d1a6e308e5720d27860f3bbff1184
Ville <ville.o.laitinen@edu.turkuamk.fi>
2022-11-30 00:27:29 +0200

    10.11 complete

---
+++ src/graphql/fragments.js
@@ -0,0 +1,1 @@
+// Possible fragments here
---
+++ src/hooks/useRepositories.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_REPOSITORIES } from "../graphql/queries";
+
+const useRepositories = () => {
+  const [repositories, setRepositories] = useState();
+
+  const { data, error, loading, refetch } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+  });
+
+  const fetchRepositories = () => {
+    refetch();
+  };
+
+  if (error) {
+    console.log(error);
+  }
+
+  useEffect(() => {
+    if (!loading && data) {
+      setRepositories(data.repositories);
+    }
+  }, [data]);
+
+  return {
+    repositories,
+    loading,
+    refetch: fetchRepositories,
+    hookState: repositories,
+  };
+};
+
+export default useRepositories;
