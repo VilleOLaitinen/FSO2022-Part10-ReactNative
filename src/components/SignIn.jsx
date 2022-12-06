4e71d7fb6aaeaf0ad2b34729d25e6216891d719d
Ville <ville.o.laitinen@edu.turkuamk.fi>
2022-12-06 02:24:39 +0200

    10.15 complete

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -5,6 +5,7 @@ import { Formik } from "formik";
 import FormikTextInput from "./FormikTextInput";
 import * as yup from "yup";
 import useSignIn from "../hooks/useSignIn";
+import { useNavigate } from "react-router-native";
 
 const styles = StyleSheet.create({
   container: {
@@ -71,8 +72,8 @@ const SignInForm = ({ onSubmit }) => {
 };
 
 const SignIn = () => {
-
   const [signIn] = useSignIn();
+  const navigate = useNavigate();
 
   const onSubmit = async (values) => {
     const { username, password } = values;
@@ -80,6 +81,9 @@ const SignIn = () => {
     try {
       const { data } = await signIn({ username, password });
       console.log(data);
+      if (data) {
+        navigate("/");
+      }
     } catch (e) {
       console.log(e);
     }
--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,19 +1,31 @@
 import { useMutation } from "@apollo/client";
 import { AUTHENTICATE } from "../graphql/queries";
+import useAuthStorage from "./useAuthStorage";
+import { useApolloClient } from "@apollo/client";
 
 const useSignIn = () => {
+  const client = useApolloClient();
+  const authStorage = useAuthStorage();
   const [mutate, result] = useMutation(AUTHENTICATE, {
     onError: (error) => {
       console.log(error.graphQLErrors[0].message);
     },
   });
 
-  const signIn = async ({username, password}) => {
+  const signIn = async ({ username, password }) => {
     const credentials = { username, password };
-    return await mutate({ variables: {credentials} });
+    const res = await mutate({ variables: { credentials } });
+    if (res.data.authenticate.accessToken) {
+      const token = res.data.authenticate.accessToken;
+      await authStorage.setAccessToken(token);
+      client.resetStore();
+
+      return res;
+    }
+    return null;
   };
 
   return [signIn, result];
-}
+};
 
 export default useSignIn;
